81d8ad38748610f2b1496ebf8f77d48d9f5aa639
twiessner <22835957+twiessner@users.noreply.github.com>
2022-10-10 21:27:12 +0200

    minor fixes on aks cluster

--- infrastructure/azure/terraform/modules/aks/resource-group.tf
+++ infrastructure/azure/terraform/modules/aks/resource-group.tf
@@ -1,5 +1,5 @@
 
 resource "azurerm_resource_group" "aks" {
-  name     = join("", ["rg", var.scope, "aks", var.location.short])
+  name     = join("-", ["rg", var.scope, "aks", var.location.short])
   location = var.location.name
 }
